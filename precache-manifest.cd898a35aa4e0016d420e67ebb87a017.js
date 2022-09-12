7f8326a041ccd3b56ad7fc0a661ef40d2cbab96c
jurena <jurena@microstrategy.com>
2022-09-12 16:28:21 +0200

    Updates

---
+++ precache-manifest.cd898a35aa4e0016d420e67ebb87a017.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4eb11e434e45eb3abc5214b3fe8bf435",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a3e6a45f6f1450b0293a",
+    "url": "./static/css/main.b693b373.chunk.css"
+  },
+  {
+    "revision": "6b93691c2f4805dae432",
+    "url": "./static/js/2.04dc567d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.04dc567d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3e6a45f6f1450b0293a",
+    "url": "./static/js/main.4f67ce81.chunk.js"
+  },
+  {
+    "revision": "af32d328258fb1d29f98",
+    "url": "./static/js/runtime-main.648a9e93.js"
+  }
+]);
